6d676047df7a16b819a5259b51e39aaa0816d38e
Nishan Karki <karki.nishann@gmail.com>
2023-04-06 21:30:51 +0200

    changes

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import requests
 import credentials
+from kafka import KafkaProducer
+import time
 
 BASE_URL = "https://api.currencyfreaks.com/v2.0/rates/latest?"
 API_KEY = credentials.key
-# https://api.currencyfreaks.com/v2.0/rates/latest?apikey=179befac34da4043bd6f00b7e48b5753
 response = requests.get(f"https://api.currencyfreaks.com/v2.0/rates/latest?apikey={API_KEY}")
-print(response.status_code)
+
+
+
+data = response.json()
+base = data['base']
+rates = {}
+currencies = data["rates"]
+for k,v in currencies.items():
+    rates[k] = v
+
+producer = KafkaProducer(bootstrap_servers ="localhost:9093", acks=1)
+producer.send("currency-topic", bytes(base, 'utf-8'))
+producer.flush()
