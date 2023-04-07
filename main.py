7bd1f5cc23d71c939374d675086a360e18a042ba
Nishan Karki <karki.nishann@gmail.com>
2023-04-07 04:38:26 +0200

    changes

--- main.py
+++ main.py
@@ -3,24 +3,27 @@ import credentials
 from kafka import KafkaProducer
 import time
 import json
+import pandas as pd
+# BASE_URL = "https://api.currencyfreaks.com/v2.0/rates/latest?"
+# API_KEY = credentials.key
 
-BASE_URL = "https://api.currencyfreaks.com/v2.0/rates/latest?"
-API_KEY = credentials.key
+# i = 0
+# while i <= 4:
+#     response = requests.get(f"https://api.currencyfreaks.com/v2.0/rates/latest?apikey={API_KEY}")
+#     data = response.json()
+#     base = data['base']
+#     rates = {}
+#     currencies = data["rates"]
+#     for k,v in currencies.items():
+#         rates[k] = v
 
-i = 0
-while i <= 4:
-    response = requests.get(f"https://api.currencyfreaks.com/v2.0/rates/latest?apikey={API_KEY}")
-    data = response.json()
-    base = data['base']
-    rates = {}
-    currencies = data["rates"]
-    for k,v in currencies.items():
-        rates[k] = v
+#     dict_data = {base:rates}
+#     kafka_data = json.dumps(dict_data)
+#     producer = KafkaProducer(bootstrap_servers ="localhost:9093", acks=1)
+#     producer.send("currency-topic", bytes(kafka_data, 'utf-8'))
+#     time.sleep(12)
+#     i += 1
+#     producer.flush()
 
-    dict_data = {base:rates}
-    kafka_data = json.dumps(dict_data)
-    producer = KafkaProducer(bootstrap_servers ="localhost:9093", acks=1)
-    producer.send("currency-topic", bytes(kafka_data, 'utf-8'))
-    time.sleep(12)
-    i += 1
-    producer.flush()
+
+demo = pd.read_csv()
--- transformation.py
+++ transformation.py
@@ -1,4 +1,18 @@
 import pandas as pd
 
+def add_ticker(path, ticker):
+    df = pd.read_csv(f"{path}")
+    df['Stock'] = ticker
+    print(df.head())
+    df.to_csv(f"{path}")
 
-df = pd.read_csv("./meta.csv")
+add_ticker(path="data/amazon.csv", ticker="AMZN")
+add_ticker(path="data/apple.csv", ticker="AAPL")
+add_ticker(path="data/coke.csv", ticker="COKE")
+add_ticker(path="data/google.csv", ticker="GOOG")
+add_ticker(path="data/ibm.csv", ticker="IBM")
+add_ticker(path="data/johnson.csv", ticker="JNJ")
+add_ticker(path="data/meta.csv", ticker="META")
+add_ticker(path="data/nvidia.csv", ticker="NVDA")
+add_ticker(path="data/pinterest.csv", ticker="PINS")
+add_ticker(path="data/tesla.csv", ticker="TSLA")
